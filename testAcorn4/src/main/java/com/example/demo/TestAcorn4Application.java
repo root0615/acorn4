a9908771ecdad72a82fabbfea9e7d07a0584ac74
GROOT <yungun4101@gmail.com>
2023-03-28 10:32:35 +0900

    test git

---
+++ testAcorn4/src/main/java/com/example/demo/TestAcorn4Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAcorn4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestAcorn4Application.class, args);
+	}
+
+}
